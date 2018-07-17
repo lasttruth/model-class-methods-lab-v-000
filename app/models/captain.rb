d1483f01b547d7764bdecbca84a195cfadd005ea
Robin Doirin <robindoirin@gmail.com>
2018-07-17 18:00:57 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -6,15 +6,15 @@ class Captain < ActiveRecord::Base
   end
 
   def self.dinghy
-
+    where("length < 20")
   end
 
   def self.ship
-
+    where("length >= 20 ")
   end
 
   def self.last_three_alphabetically
-
+    all.order(name: :desc).limit(3)
   end
 
   def self.without_a_captain
