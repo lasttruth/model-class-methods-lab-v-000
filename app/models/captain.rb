f4999fcdde2d54c3792c45244650a960a07552d5
Robin Doirin <robindoirin@gmail.com>
2018-07-17 17:55:21 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,27 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def seld.first_five
+    
+  end
+
+  def dinghy
+    
+  end
+
+  def last_three_alphabetically 
+    
+  end
+
+  def without_a_captain
+    
+  end
+
+  def sailboats
+    
+  end
+
+  def with_three_classifications
+    
+  end
 end
